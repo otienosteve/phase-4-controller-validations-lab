867b4df499866b91e607ed347239418a3b13a1cf
otienosteve <steveotieno701@gmail.com>
2022-09-08 16:14:07 +0300

    Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,21 @@
 class PostsController < ApplicationController
+# rescue_from  ActiveRecord::RecordInvalid, with:error_message
+# rescue_from ActiveRecord::RecordNotFound, with: error_notfound
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    post.update!(post_params)
+    render json: post , status: :ok
+    rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
+  
   private
 
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "is not included in the list" }
+    validates :content, length: {minimum: 100}
 end
